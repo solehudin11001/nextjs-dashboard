8a188f8f1deb50788f26b9cccc1d603896e73b05
solehudin11001 <solehudin11001@gmail.com>
2025-01-09 14:08:13 +0700

    fix: remove unused vars

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -3,7 +3,6 @@
 import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { deleteInvoice } from "@/app/lib/actions";
-import { revalidatePath } from "next/cache";
 
 export function CreateInvoice() {
   return (
